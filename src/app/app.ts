71e90376c40da2389c6f7f2107d15ff8c145a606
Agustin Jose Farisano <farisanoagustin@gmail.com>
2026-07-03 23:49:15 -0300

    add a tokens css styles import and create button and card components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Button } from './shared/ui/components/button/button';
+import { ButtonComponent } from './shared/ui/components/button/button';
+import { Card } from './shared/ui/components/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, ButtonComponent, Card],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/shared/ui/components/button/button.ts
+++ src/app/shared/ui/components/button/button.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './button.html',
   styleUrl: './button.css',
 })
-export class Button {}
+export class ButtonComponent {}
